8e2aa87bc1fc35edda3eb41ca81e059eda2d4b86
alwanof <free1soft@gmail.com>
2021-12-08 13:55:37 +0300

    3rd commit

--- test/home.js
+++ test/home.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 describe('Home', function() {
     it('Testo',  function() {
-        assert(true,'alert message has been received'); 
+        assert(false,'alert message has been received'); 
     });
 });
+//  - run: npm run build --if-present
+//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
